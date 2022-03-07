128c34c8fc94b9aa2538f86010c0b12d39fde98f
ChenXander <cx1197257655@gmail.com>
2022-03-07 19:38:25 +0800

    登录token

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
+const TOKEN_KEY = 'TOUTIAO_USER'
+
 export default new Vuex.Store({
   state: {
+    //   存储当前登录用户信息（token等数据）
+    // user: JSON.parse(window.localStorage.getItem(TOKEN_KEY)),
+    user: getItem(TOKEN_KEY)
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setUser (state, data) {
+      state.user = data
+      //   防止刷新丢失，数倍备份到本地存储
+      // window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      setItem(TOKEN_KEY, state.data)
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  actions: {},
+  modules: {}
 })
