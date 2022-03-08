4c0f117ceb5a235c5ecd3fbbea96e90b61051102
ChenXander <cx1197257655@gmail.com>
2022-03-08 09:14:31 +0800

    我的页面头部布局(已登录)

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,11 +1,52 @@
 <template>
   <div class="my-container">
+    <!-- 未登录头部 -->
     <div class="header not-login">
       <div class="login-btn" @click="$router.push('/login')">
         <img class="mobile-img" src="~@/assets/mobile.png" alt="" />
         <span class="text">登录 / 注册</span>
       </div>
     </div>
+    <!-- /未登录头部 -->
+
+    <!-- 已登录头部 -->
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image
+            class="avatar"
+            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+            round
+            fit="cover"
+          />
+          <span class="name">我的头条号</span>
+        </div>
+
+        <div class="right">
+          <van-button size="mini" round>编辑资料</van-button>
+        </div>
+      </div>
+
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">66</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+      </div>
+    </div>
+    <!-- /已登录头部 -->
   </div>
 </template>
 
@@ -45,6 +86,51 @@ export default {
       .text {
         font-size: 28px;
         color: #fff;
+      }
+    }
+  }
+
+  .user-info {
+    .base-info {
+      height: 231px;
+      padding: 76px 32px 23px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+        display: flex;
+        align-items: center;
+        //   用户头像
+        .avatar {
+          width: 132px;
+          height: 132px;
+          border: 4px solid #fff;
+          margin-right: 23px;
+        }
+        //   用户名
+        .name {
+          font-size: 30px;
+          color: #fff;
+        }
+      }
+    }
+    .data-stats {
+      display: flex;
+      .data-item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 130px;
+        flex: 1;
+        color: #fff;
+        .count {
+          font-size: 36px;
+        }
+        .text {
+          font-size: 23px;
+        }
       }
     }
   }
