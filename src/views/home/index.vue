db5c82b93f3f0e50a3bfcb91d7fc3b384cad1173
ChenXander <cx1197257655@gmail.com>
2022-03-08 20:17:00 +0800

    获取频道列表展示数据

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,13 +8,7 @@
 
     <!-- 频道列表 -->
     <van-tabs class="channel-tabs" v-model="active" animated swipeable>
-      <van-tab title="标签 1">内容 1</van-tab>
-      <van-tab title="标签 2">内容 2</van-tab>
-      <van-tab title="标签 3">内容 3</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
-      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab :title="channel.name" v-for="channel in channels" :key="channel.id">{{ channel.name }}</van-tab>
       <div slot="nav-right" class="placeholder"></div>
       <div slot="nav-right" class="hamburger-btn">
         <i class="toutiao toutiao-gengduo"></i>
@@ -25,16 +19,30 @@
 </template>
 
 <script>
+import { getUserChannels } from '@/api/user'
+
 export default {
   name: 'HomeIndex',
   components: {},
   data() {
     return {
-      active: 0
+      active: 0,
+      channels: [] // 频道列表
     }
   },
-  created() {},
-  methods: {}
+  created() {
+    this.loadChannels()
+  },
+  methods: {
+    async loadChannels() {
+      try {
+        const { data } = await getUserChannels()
+        this.channels = data.data.channels
+      } catch (err) {
+        this.$toast('获取频道数据失败')
+      }
+    }
+  }
 }
 </script>
 
