b69ffd75eb2ccd8e1e8c86d334cf2ae4d688a70b
ChenXander <cx1197257655@gmail.com>
2022-03-08 10:47:53 +0800

    个人中心完成&配置请求拦截器

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,6 @@
 // 用户相关请求模块
 import request from '@/utils/request'
+// import store from "@/store";
 
 // 登录
 export const login = (data) => {
@@ -15,5 +16,18 @@ export const sendSms = (mobile) => {
   return request({
     method: 'GET',
     url: `/v1_0/sms/codes/${mobile}`
+  })
+}
+
+// 获取用户信息
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/v1_0/user'
+
+    /* 请求头，通过配置请求拦截器取代此处
+    headers: {
+      Authorization: `Bearer ${store.state.user.token}`,
+    }, */
   })
 }
