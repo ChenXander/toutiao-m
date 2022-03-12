e0cc64dbc5ef52c41e0d854232e952964b61235f
ChenXander <cx1197257655@gmail.com>
2022-03-12 20:10:50 +0800

    响应拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,16 @@
 // 统一封装接口方法
 // 每个方法负责请求一个url，便于统一管理
-import request from '@/utils/request.js'
+import axios from '@/utils/request.js'
 import { getToken } from '@/utils/token.js'
 
 // 获取所有频道
-export const getAllChannelsAPI = () => request({
+export const getAllChannelsAPI = () => axios({
   url: '/v1_0/channels',
   method: 'GET'
 })
 
 // 获取用户频道,若没有登录则默认返回后台设置的默认频道列表
-export const getUserChannelsAPI = () => request({
+export const getUserChannelsAPI = () => axios({
   url: '/v1_0/user/channels',
   method: 'GET',
   headers: {
@@ -19,7 +19,7 @@ export const getUserChannelsAPI = () => request({
 })
 
 // 获取文章推荐数据
-export const getAllArticleListAPI = ({ channel_id, timestamp }) => request({
+export const getAllArticleListAPI = ({ channel_id, timestamp }) => axios({
   url: '/v1_0/articles',
   method: 'GET',
   headers: {
@@ -33,7 +33,7 @@ export const getAllArticleListAPI = ({ channel_id, timestamp }) => request({
 
 // 文章反馈面板-不感兴趣
 export const dislikeArticleAPI = ({ artId }) => {
-  return request({
+  return axios({
     url: '/v1_0/article/dislikes',
     method: 'POST',
     headers: {
@@ -47,7 +47,7 @@ export const dislikeArticleAPI = ({ artId }) => {
 
 // 文章反馈面板-反馈垃圾内容
 export const reportArticleAPI = ({ artId, type }) => {
-  return request({
+  return axios({
     url: '/v1_0/article/reports',
     method: 'POST',
     headers: {
@@ -62,7 +62,7 @@ export const reportArticleAPI = ({ artId, type }) => {
 }
 
 // 登录接口
-export const loginAPI = ({ mobile, code }) => request({
+export const loginAPI = ({ mobile, code }) => axios({
   url: '/v1_0/authorizations',
   method: 'POST',
   data: {
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,40 @@
 // 请求模块
 import theAxios from 'axios'
+import router from '@/router'
+import { Notify } from 'vant'
 
-const request = theAxios.create({
+const axios = theAxios.create({
   baseURL: 'http://toutiao.itheima.net/', // 接口的基准路径
   timeout: 20000 // 请求超时时间
 })
 
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么,状态码2xx,3xx
+  return response
+}, function (error) {
+  // 对响应错误做点什么,状态码4xx,5xx
+  if (error.response.status === 401) {
+    // * 不能使用this.$router(因为this不是vue组件对象无法调用$router)
+    // * 导入router解决
+    Notify({ type: 'warning', message: '身份已过期' })
+    router.replace('/login')
+  }
+
+  return Promise.reject(error)
+})
+
 export default ({ url, method = 'GET', params = {}, data = {}, headers = {} }) => {
-  return request({
+  return axios({
     url,
     method,
     params,
