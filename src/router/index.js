4d890d617f79c4416b29e1cd3cc10bffbef97675
ChenXander <cx1197257655@gmail.com>
2022-03-16 17:00:25 +0800

    优化：路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '@/views/Login'
-import Layout from '@/views/Layout'
-import Home from '@/views/Home'
-import User from '@/views/User'
-import Search from '@/views/Search'
-import SearchResult from '@/views/Search/SearchResult'
-import ArticleDetail from '@/views/ArticleDetail'
-import UserEdit from '@/views/User/UserEdit'
-import Chat from '@/views/Chat'
+// import Login from '@/views/Login'
+// import Layout from '@/views/Layout'
+// import Home from '@/views/Home'
+// import User from '@/views/User'
+// import Search from '@/views/Search'
+// import SearchResult from '@/views/Search/SearchResult'
+// import ArticleDetail from '@/views/ArticleDetail'
+// import UserEdit from '@/views/User/UserEdit'
+// import Chat from '@/views/Chat'
 
+// 路由懒加载：为了让第一个页面加载的app.js小一点，打开网页快一点
+// 把组件对应的js分成若干个js，路由切换到哪个页面才加载对应的js文件
+// 原因：webpack分析入口时，直接打包了router所import的所有页面进app.js，导致app.js很大
+// 解决：当路由路径规则匹配时，才去import对应的组件js文件
 Vue.use(VueRouter)
 
 const routes = [
@@ -19,43 +23,52 @@ const routes = [
   },
   {
     path: '/login',
-    component: Login
+    component: () =>
+      import(/* webpackChunkName: "Login" */ '@/views/Login')
   },
   {
     path: '/layout',
-    component: Layout,
+    component: () =>
+      import(/* webpackChunkName: "Layout" */ '@/views/Layout'),
     children: [
       {
         path: 'home',
-        component: Home
+        component: () =>
+          import(/* webpackChunkName: "Home" */ '@/views/Home')
       },
       {
         path: 'user',
-        component: User
+        component: () =>
+          import(/* webpackChunkName: "User" */ '@/views/User')
       }
 
     ]
   },
   {
     path: '/search',
-    component: Search
+    component: () =>
+      import(/* webpackChunkName: "Search" */ '@/views/Search')
   },
   { // 搜索结果
     path: '/search_result/:kw',
-    component: SearchResult
+    component: () =>
+      import(/* webpackChunkName: "SearchResult" */ '@/views/Search/SearchResult')
   },
   // 文章详情页
   {
     path: '/detail',
-    component: ArticleDetail
+    component: () =>
+      import(/* webpackChunkName: "ArticleDetail" */ '@/views/ArticleDetail')
   },
   {
     path: '/user_edit',
-    component: UserEdit
+    component: () =>
+      import(/* webpackChunkName: "UserEdit" */ '@/views/User/UserEdit')
   },
   {
     path: '/chat',
-    component: Chat
+    component: () =>
+      import(/* webpackChunkName: "Chat" */ '@/views/Chat')
   }
 ]
 
