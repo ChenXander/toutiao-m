e5a594372ea318f2fa368a11a095c67c2f0a8f70
ChenXander <cx1197257655@gmail.com>
2022-03-16 19:30:45 +0800

    优化：文章详情代码高亮引入highlight.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'highlight.js/styles/default.css' // 代码高亮样式
 import 'amfe-flexible' // 移动端适配
 import {
   NavBar,
